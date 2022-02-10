b54f8fc98b130d2161cd9fdb248fe0ea16120a6e
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-02-10 20:41:47 -0300

    AddBuilderPatterns

---
+++ Factory/Builder/Componentes/Cambio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Componentes
+{
+    enum Cambio
+    {
+        MANUAL,
+        AUTOMATICO,
+        DUAL_LOGIC,
+    }
+}
---
+++ Factory/Builder/Componentes/Motor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder.Componentes
+{
+    class Motor
+    {
+        private int ligar;
+        public int Ligar { get; set; }
+    }
+}

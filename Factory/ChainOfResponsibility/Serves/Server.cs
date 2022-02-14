6b839bbf6e5f16453ca5d96c20de5539d2985619
Cleidson <cleidsonestevamdasilva@hotmail.com>
2022-02-14 20:00:35 -0300

    AddChainofResponsibility

---
+++ Factory/ChainOfResponsibility/Serves/Server.cs
@@ -0,0 +1,44 @@
+﻿using ChainOfResponsibility.Middlewares;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibility.Serves
+{
+    class Server
+    {
+        private Dictionary<string, string> users = new Dictionary<string, string>();
+        private Middleware middleware;
+
+        public void SetMiddeware(Middleware middlewares) 
+        {
+            this.middleware = middleware;
+        }
+
+        public Boolean Login(string email, string password) 
+        {
+            if (middleware.Check(email, password)) 
+            {
+              Console.WriteLine("Usuario autorizado");
+                return true;
+            }
+            return false;
+        }
+        public void RegisterUser(string email, string password)
+        {
+            users[email] = password;
+        }
+        public Boolean HasEmail(string email)
+        {
+            return users.ContainsKey(email);
+        }
+        public Boolean IsValidPassword(string email, string password)
+        {
+            string value = "";
+            users.TryGetValue(email, out value);
+            return password == value;
+        }
+
+
+    }
+}
